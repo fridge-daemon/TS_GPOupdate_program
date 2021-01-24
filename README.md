17133a587ab48328bf6712ae57576d2ec936cbd3
O'Brien <Spencer.OBrien@lcb.state.nv.us>
2021-01-23 19:30:04 -0800

    Added firstscript.py
    Created first script

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # didactic-succotash
+this is a test repo for learning how to use git/github
---
+++ firstscript.py
@@ -0,0 +1,7 @@
+
+variable1="Guy standing on street corner... "
+variable2="two dumb college kids driving in a car..."
+Initialreasoning="initiation of public humiliation and self deprication..."
+FinalResult="....................Yeah riiiiiggghhhhtttt!!!!"
+
+print(variable1+"PLUS "+variable2+"TIMES "+Initialreasoning+"EQUALS "+FinalResult)
