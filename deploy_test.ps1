3ce9a26e12b0b9be980538a57ade5c7713436447
William Lake <William.Lake@mt.gov>
2021-02-17 12:01:02 -0700

    Providing Feedback
    Moved the lines saving the ouptut of `copy-item` for a couple reasons:
    
    	- `copy-item` looks like it doesn't provide output unless the -PassThru
    	argument is used (More info
    	here)[https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/copy-item?view=powershell-7.1#parameters]
    	- It looks like the item should only be copied after verifying
    	the source directories exist- and calling `copy-item`/saving the
    	output would have copied the item right away (I.e. before there
    	was a chance to check whether the source dirs existed.)
    
    Combined the nested if statement into an elseif for no other reason than
    personal preference/readability.
    
    Added an extra else statement to provide user output if no files could
    be copied.
    
    Added some code comments, which I _should_ say is a best practice- but
    don't really have a leg to stand on b/c I rarely do it. (Although it
    does help future-you remember your mindset.)

--- deploy_test.ps1
+++ deploy_test.ps1
@@ -4,15 +4,19 @@ $source = "\\nt9\spencer.obrien$\SHARES\Timesheet\*"
 $path1 = "c:\TEST\new1"
 $path2= "c:\TEST\new2"
 
+# Check if the test paths exists.
 $new1_test = test-path($path1)
-$new1_Install = copy-item -path $source -Destination c:\\TEST\new1\
 $new2_test = test-path($path2)
-$new2_Install = copy-item -path $source -destination c:\\TEST\new2\
 
+# If the first path exists, copy the files to it.
 if($new1_test){
-    $new1_Install
-}else {
-    if ($new2_test){
-        $new2_Install
-    }
+    copy-item -path $source -Destination $path1
+
+# Otherwise, if the second path exists- copy the files there.
+}elseif ($new2_test){
+    copy-item -path $source -destination $path2
+
+# Otherwise alert the user.
+} else {
+    Write-Output "Neither ${path1} nor ${path2} exist!"
 }
--- prep_test.ps1
+++ prep_test.ps1
@@ -7,14 +7,17 @@ $source2contents = "c:\TEST\new2\*"
 $dest = "\\nt9\spencer.obrien$\SHARES\"
 
 $source1_test = test-path($source1)
-$source1_Install = copy-item -path $source1contents -destination $dest
 $source2_test = test-path($source2)
-$source2_Install = copy-item -path $source2contents -destination $dest
 
+# If source1 exists, then copy the files.
 if($source1_test){
-    $source1_Install
-}else {
-    if ($source2_test){
-        $source2_Install
-    }
+    copy-item -path $source1contents -destination $dest
+
+# Otherwise, if source 2 exists copy the files there.
+}elseif ($source2_test){
+    copy-item -path $source2contents -destination $dest
+
+# Otherwise alert the user.
+} else {
+    Write-Output "Neither ${source1} nor ${source2} exist!"
 }
